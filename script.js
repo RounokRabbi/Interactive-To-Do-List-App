3aee70a0c9bd9b52094d4120f4edc429051720f8
RounokRabbi <rounok.cse.20220104096@aust.edu>
2024-05-19 10:51:14 +0600

    js update

--- script.js
+++ script.js
@@ -32,11 +32,45 @@ function saveData() {
 
 function showList() {
     listContainer.innerHTML = localStorage.getItem("data");
+    let lis = listContainer.querySelectorAll("li");
+    lis.forEach(addEditListener);
 }
 
 function clearAll() {
     listContainer.innerHTML = "";
     saveData();
 }
+function addEditListener(li) {
+    li.addEventListener("dblclick", function() {
+        let currentText = li.childNodes[0].nodeValue;
+        let input = document.createElement("input");
+        input.type = "text";
+        input.value = currentText;
+        li.innerHTML = "";
+        li.appendChild(input);
+        input.focus();
+
+        input.addEventListener("blur", function() {
+            li.innerHTML = input.value;
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
+            addEditListener(li);
+            saveData();
+        });
+
+        input.addEventListener("keypress", function(e) {
+            if (e.key === "Enter") {
+                li.innerHTML = input.value;
+                let span = document.createElement("span");
+                span.innerHTML = "\u00d7";
+                li.appendChild(span);
+                addEditListener(li);
+                saveData();
+            }
+        });
+    });
+}
+
 
 showList();
